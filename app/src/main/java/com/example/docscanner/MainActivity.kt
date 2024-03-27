c273b28c2e97299ec8ac20230a295b8fab7c76d1
Anu-Basu <119494632+Anu-Basu@users.noreply.github.com>
2024-03-27 23:20:02 +0530

    added files

--- app/src/main/java/com/example/docscanner/MainActivity.kt
+++ app/src/main/java/com/example/docscanner/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.example.docscanner
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.docscanner.ui.theme.DocScannerTheme
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val options = GmsDocumentScannerOptions.Builder()
+            .setGalleryImportAllowed(true)
+            .setScannerMode(SCANNER_MODE_FULL)
+            .setPageLimit(50)
+            .setResultFormats(RESULT_FORMAT_PDF, RESULT_FORMAT_JPEG)
+            .build()
+        val scnaner = GmsDocumentScanning.getClient(options)
 
         setContent {
             DocScannerTheme {
@@ -20,6 +39,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val imageUris = remember{
+                        mutableStateListOf<Uri>()
+                    }
+                    val scannerLancher = rememberLauncherForActivityResult(
+                        contract = ActivityResultContracts.StartActivityForResult(),
+                        onResult ={
+                        }
+                    )
+                    Column(
+
+                    )
+                    {
+
+                    }
                 }
             }
         }
